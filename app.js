f31e37d2097da48e0177c131847611f1505052a6
ryan woods <federer5050@gmail.com>
2020-12-14 18:23:01 +0000

    menu buttons added with dataset

--- app.js
+++ app.js
@@ -82,9 +82,32 @@ const menu = [
   ];
 
   const sectionCenter = document.querySelector('.section-center');
+  const filterBtns = document.querySelectorAll('.filter-btn');
 
+//   load items
   window.addEventListener('DOMContentLoaded',function() {
-      let displayMenu = menu.map(function(item){
+     displayMenuItems(menu);
+  });
+//   filter items
+filterBtns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+          if (menuItem.category === category) {
+            return menuItem;
+          }
+        });
+          if (category === "all") {
+            displayMenuItems(menu);
+          } else {
+            displayMenuItems(menuCategory);
+          }
+      })
+    });
+
+
+  function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function(item){
         //   console.log(item.title);
 
           return `<article class="menu-item">
@@ -102,5 +125,4 @@ const menu = [
       });
       displayMenu = displayMenu.join("");
       sectionCenter.innerHTML = displayMenu
-      console.log(displayMenu);
-  });
+  }
